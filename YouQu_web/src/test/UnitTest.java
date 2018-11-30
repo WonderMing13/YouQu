7caebcf9235bf82510976df144a074c988a6d304
wonderming13 <550207385@qq.com>
2018-11-30 10:06:59 +0800

    angularJS模板1.7.5

---
+++ YouQu_web/src/test/UnitTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: 王德明
+ * @Date: 2018/11/29
+ * @Time: 13:47
+ * @Project: youqu
+ * @Package: PACKAGE_NAME
+ */
+public class UnitTest {
+
+        @Test
+        public void poJoTest() {
+            int i = 0;
+            do {
+               i++;
+            }while (true);
+        }
+}
